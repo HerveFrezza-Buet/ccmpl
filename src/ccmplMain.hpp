e10be0222b6786b0fbf24eea76c862a308d0aa18
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2018-09-18 08:44:12 +0200

    Adds the possibility to change the movie_writer.

--- src/ccmplMain.hpp
+++ src/ccmplMain.hpp
@@ -7,9 +7,12 @@ namespace ccmpl {
   private:
     bool generate_mode, movie;
     std::string pyfile, moviefile;
-
+    std::string movie_writer;
+    
     Main(const std::string& prefix)
-      : pyfile(prefix+".py"), moviefile(prefix+".mp4") {}
+      : pyfile(prefix+".py"),
+	moviefile(prefix+".mp4"),
+	movie_writer("avconv") {}
 
   public:
 
@@ -42,10 +45,16 @@ namespace ccmpl {
       this->movie         = movie;
     }
 
+    // Change the movie writer used when generating the python file
+    // This function must be called before "generate" to be effective
+    void set_movie_writer(std::string writer) {
+      movie_writer = writer;
+    }
+    
     void generate(ccmpl::chart::Layout& display, bool use_gui) {
       if(generate_mode) {
 	if(movie)
-	  display.make_movie_python(pyfile,use_gui, "avconv", "", "", "", 25, moviefile, 300);
+	  display.make_movie_python(pyfile,use_gui, movie_writer, "", "", "", 25, moviefile, 300);
 	else
 	  display.make_python(pyfile,use_gui); 
 	std::exit(0);                            
